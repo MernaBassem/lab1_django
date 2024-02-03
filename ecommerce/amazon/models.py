5b412466ebe00dd7d1a699c0751a439ead901ab0
Merna Bassem <mernabassem21@gmail.com>
2024-02-03 20:35:51 +0200

    add , update , delete and select By name product from or to datebase

--- ecommerce/amazon/models.py
+++ ecommerce/amazon/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.CharField(max_length=150)
+    active = models.BooleanField(default=True)
+    
+    
+    def __str__(self) :
+        return self.title
--- ecommerce/amazon/views.py
+++ ecommerce/amazon/views.py
@@ -1,207 +1,59 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-products = [
- {
-"id": 1,
-"title": "iPhone 9",
-"price": 549,
-"category": "smartphones",
-"thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg",
-
-},
-{
-"id": 2,
-"title": "iPhone X",
-"price": 899,
-"category": "smartphones",
-"thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg",
-},
-{
-"id": 3,
-"title": "Samsung Universe 9",
-"price": 1249,
-"category": "smartphones",
-"thumbnail": "https://cdn.dummyjson.com/product-images/3/thumbnail.jpg",
-},
-{
-"id": 4,
-"title": "OPPOF19",
-"price": 280,
-"category": "smartphones",
-"thumbnail": "https://cdn.dummyjson.com/product-images/4/thumbnail.jpg",
-},
-{
-"id": 5,
-"title": "Huawei P30",
-"price": 499,
-"category": "smartphones",
-"thumbnail": "https://cdn.dummyjson.com/product-images/5/thumbnail.jpg",
-
-},
-{
-"id": 6,
-"title": "MacBook Pro",
-"price": 1749,
-"category": "laptops",
-"thumbnail": "https://cdn.dummyjson.com/product-images/6/thumbnail.png",
-},
-{
-"id": 7,
-"title": "Samsung Galaxy Book",
-"price": 1499,
-"category": "laptops",
-"thumbnail": "https://cdn.dummyjson.com/product-images/7/thumbnail.jpg",
-},
-{
-"id": 8,
-"title": "Microsoft Surface Laptop 4",
-"price": 1499,
-"category": "laptops",
-"thumbnail": "https://cdn.dummyjson.com/product-images/8/thumbnail.jpg",
-},
-{
-"id": 9,
-"title": "Infinix INBOOK",
-"price": 1099,
-"category": "laptops",
-"thumbnail": "https://cdn.dummyjson.com/product-images/9/thumbnail.jpg",
-},
-{
-"id": 10,
-"title": "HP Pavilion 15-DK1056WM",
-"price": 1099,
-"category": "laptops",
-"thumbnail": "https://cdn.dummyjson.com/product-images/10/thumbnail.jpeg",
-},
-{
-"id": 11,
-"title": "perfume Oil",
-"price": 13,
-"category": "fragrances",
-"thumbnail": "https://cdn.dummyjson.com/product-images/11/thumbnail.jpg",
-},
-{
-"id": 12,
-"title": "Brown Perfume",
-"price": 40,
-"category": "fragrances",
-"thumbnail": "https://cdn.dummyjson.com/product-images/12/thumbnail.jpg",
-
-},
-{
-"id": 13,
-"title": "Fog Scent Xpressio Perfume",
-"price": 13,
-"category": "fragrances",
-"thumbnail": "https://cdn.dummyjson.com/product-images/13/thumbnail.webp",
-
-},
-{
-"id": 14,
-"title": "Non-Alcoholic Concentrated Perfume Oil",
-"price": 120,
-"category": "fragrances",
-"thumbnail": "https://cdn.dummyjson.com/product-images/14/thumbnail.jpg",
-},
-{
-"id": 15,
-"title": "Eau De Perfume Spray",
-"price": 30,
-"category": "fragrances",
-"thumbnail": "https://cdn.dummyjson.com/product-images/15/thumbnail.jpg",
-},
-{
-"id": 16,
-"title": "Hyaluronic Acid Serum",
-"price": 19,
-"category": "skincare",
-"thumbnail": "https://cdn.dummyjson.com/product-images/16/thumbnail.jpg",
-},
-{
-"id": 17,
-"title": "Tree Oil 30ml",
-"price": 12,
-"category": "skincare",
-"thumbnail": "https://cdn.dummyjson.com/product-images/17/thumbnail.jpg",
-
-},
-{
-"id": 18,
-"title": "Oil Free Moisturizer 100ml",
-"price": 40,
-"category": "skincare",
-"thumbnail": "https://cdn.dummyjson.com/product-images/18/thumbnail.jpg",
-
-},
-{
-"id": 19,
-"title": "Skin Beauty Serum.",
-"price": 46,
-"category": "skincare",
-"thumbnail": "https://cdn.dummyjson.com/product-images/19/thumbnail.jpg",
-
-},
-{
-"id": 20,
-"title": "Freckle Treatment Cream- 15gm",
-"price": 70,
-"category": "skincare",
-"thumbnail": "https://cdn.dummyjson.com/product-images/20/thumbnail.jpg",
-
-},
-{
-"id": 21,
-"title": "- Daal Masoor 500 grams",
-"price": 20,
-"category": "groceries",
-"thumbnail": "https://cdn.dummyjson.com/product-images/21/thumbnail.png",
-
-},
-{
-"id": 22,
-"title": "Elbow Macaroni - 400 gm",
-"price": 14,
-"category": "groceries",
-"thumbnail": "https://cdn.dummyjson.com/product-images/22/thumbnail.jpg",
-
-},
-{
-"id": 23,
-"title": "Orange Essence Food Flavou",
-"price": 14,
-"category": "groceries",
-"thumbnail": "https://cdn.dummyjson.com/product-images/23/thumbnail.jpg",
-
-},
-{
-"id": 24,
-"title": "cereals muesli fruit nuts",
-"price": 46,
-"category": "groceries",
-"thumbnail": "https://cdn.dummyjson.com/product-images/24/thumbnail.jpg",
-
-}
-]
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Product
+from django.shortcuts import render, get_object_or_404
 
 def product(request):
-    context = {'products': products}
+    query = request.GET.get('search', '') 
+    
+    if query:
+        products = Product.objects.filter(title__icontains=query)
+    else:
+        products = Product.objects.all()
+
+    context = {'products': products, 'search_value': query}
     return render(request, 'pages/product.html', context)
 
-
 def add(request):
+    if(request.method=="POST"):
+        Product.objects.create(title=request.POST['name'],
+                                category=request.POST['category'],
+                                price=request.POST['price'],
+                                image=request.POST['image']
+                                )    
+        return redirect('product') 
     return render(request, 'pages/add.html')
 
 
+def delete_product(request, product_id):
+    product_to_delete = get_object_or_404(Product, pk=product_id)
+    product_to_delete.delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+
+
 def productDetail(request, productID):
-    product = next((p for p in products if p['id'] == productID), None)
+    product = next((p for p in Product.objects.all() if p.id == productID), None)
     context = {'product': product}
     if product:
         return render(request, 'pages/productDetail.html', context)
     return HttpResponse('<span style="color:red">Product not found</span>')
 
+def update(request, productID):
+    product = get_object_or_404(Product, id=productID)
+
+    if request.method == 'POST':
+        product.title = request.POST['name']
+        product.category = request.POST['category']
+        product.price = request.POST['price']
+        product.image = request.POST['image']
+        product.save()
+
+        return redirect('productDetail', productID=product.id)
+    context = {'product': product}
+    return render(request, 'pages/update.html', context)
+
 def category(request):
-    context = {'products': products}
+    context = {'products': Product.objects.all()}
     return render(request, 'pages/category.html', context)
 
 def about(request):
